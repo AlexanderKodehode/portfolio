c8ec536d3ab8e8c1e54e728bdae6c9b7674e98cf
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-09-16 14:03:04 +0200

    Updates

---
+++ precache-manifest.c4befe0fc3c9a86a188bb8dc92560f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd22c9fc179b2992071",
+    "url": "/portfolio/static/css/main.eb826c00.chunk.css"
+  },
+  {
+    "revision": "dcd22c9fc179b2992071",
+    "url": "/portfolio/static/js/main.8f5d44e1.chunk.js"
+  },
+  {
+    "revision": "0af1133c6ccd0c49cf2e",
+    "url": "/portfolio/static/js/runtime~main.4c091af5.js"
+  },
+  {
+    "revision": "109f178bcf0840d7c322",
+    "url": "/portfolio/static/js/2.9bca5a03.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/portfolio/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "809d545c83dfcd6169d3dd5ce02161ad",
+    "url": "/portfolio/index.html"
+  }
+];
