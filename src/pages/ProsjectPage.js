b3eb3e7381a4a5a578d428fa01032b110a5fcd4f
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-09-28 10:49:07 +0200

    add Background color

--- src/pages/ProsjectPage.js
+++ src/pages/ProsjectPage.js
@@ -3,7 +3,7 @@ import Prosject1 from '../components/Cards/Prosject1Card'
 import Prosject2 from '../components/Cards/Prosject2Card';
 import Prosject3 from '../components/Cards/Prosject3Card';
 import Flexbox from 'flexbox-react';
-import '../App.css';
+
 
 
 const ProsjectPage = () =>{
