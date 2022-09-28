7cf3dcd3f7f749ca4544f0f109729d49c7805527
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-09-28 12:47:44 +0200

    adding drumkit

--- src/components/Cards/Prosject1Card.js
+++ src/components/Cards/Prosject1Card.js
@@ -6,12 +6,12 @@ import styles from '../styles/my-style.module.css';
 const Prosject1 = () => {
     return(
         <Flexbox className={styles.prosject} flexDirection="column">
-            <h2 className={styles.titel}>Prosject 1</h2>
+            <h2 className={styles.titel}>Drumkit</h2>
             <img src="" alt="Profilpitcher"></img>
-            <p className={styles.titel}>Placeholder text blablablablabla</p>
+            <p className={styles.titel}>Dette er en side hvor det er lagt inn tommeset som bruker kanpper fra tastaturet til å spille av tromme lyder</p>
             <Flexbox flexDirection="row">
-                <a className={styles.demolink} href="https://www.w3schools.com" target="_blank">Live demo</a>
-                <a className={styles.gitlink} href="https://www.w3schools.com" target="_blank">Github code</a>
+                <a className={styles.demolink} href="https://alexanderkodehode.github.io/Drum-Kit/" target="_blank">Live demo</a>
+                <a className={styles.gitlink} href="https://github.com/AlexanderKodehode/Drum-Kit" target="_blank">Github code</a>
             </Flexbox>
         </Flexbox>
     );
