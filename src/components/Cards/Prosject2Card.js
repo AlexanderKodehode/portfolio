e997ecce7c2c96f6ec701c9efe33f9219facee81
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-09-28 12:59:17 +0200

    adding todo list prosject

--- src/components/Cards/Prosject2Card.js
+++ src/components/Cards/Prosject2Card.js
@@ -5,12 +5,12 @@ import styles from '../styles/my-style.module.css';
 const Prosject2 = () => {
     return(
         <Flexbox className={styles.prosject} flexDirection="column">
-            <h2>Prosject 2</h2>
+            <h2>Todo list</h2>
             <img src="" alt="Profilpitcher"></img>
-            <p>Placeholder text blablablablabla</p>
+            <p>Here har vi en Todo list</p>
             <Flexbox flexDirection="row">
-                <a className={styles.demolink} href="https://www.w3schools.com" target="_blank">Live demo</a>
-                <a className={styles.gitlink} href="https://www.w3schools.com" target="_blank">Github code</a>
+                <a className={styles.demolink} href="https://alexanderkodehode.github.io/Todo/" target="_blank">Live demo</a>
+                <a className={styles.gitlink} href="https://github.com/AlexanderKodehode/Todo" target="_blank">Github code</a>
             </Flexbox>
         </Flexbox>
     );
