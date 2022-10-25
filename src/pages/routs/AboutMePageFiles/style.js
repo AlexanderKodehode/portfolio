6214d9803c5ee22d90ccae5b7f1d24a57d8f2387
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-10-25 15:23:07 +0200

    Changes on aboutme page

--- src/pages/routs/AboutMePageFiles/style.js
+++ src/pages/routs/AboutMePageFiles/style.js
@@ -20,21 +20,25 @@ export const Right_Container = styled.div`
 `
 export const Text = styled.div`
     font-size: 30px;
+    padding: 15px 10px 10px 10px;
 `
 export const Contact = styled.div`
 
 `
 export const Contact_Titel = styled.h1`
     font-size: 75px;
+    border-bottom: lightblue 5px solid;
 `
 export const Mail_Container = styled.div`
     font-size: 22px;
+    padding: 15px 5px 10px 30px;
 `
 export const Contact_Links = styled.a`
     justify-items: space-between;
     font-size: 30px;
     display: flex;
     flex-direction: column;
+    padding: 55px 5px 60px 5px;
 `
 export const Cv = styled.button`
     background-color: var(--color-btn);
